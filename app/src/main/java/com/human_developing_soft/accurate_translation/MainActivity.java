00fc9a94f34f2bf97ac2f26c0d333e754d9a433e
shadowzilot <e.ponomaryow2017@yandex.ru>
2021-08-09 10:17:45 +0300

    Initial commit

---
+++ app/src/main/java/com/human_developing_soft/accurate_translation/MainActivity.java
@@ -0,0 +1,13 @@
+package com.human_developing_soft.accurate_translation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/human_developing_soft/accurate_translation/translation/ui/TranslationFragment.java
@@ -0,0 +1,6 @@
+package com.human_developing_soft.accurate_translation.translation.ui;
+
+import androidx.fragment.app.Fragment;
+
+public class TranslationFragment extends Fragment {
+}
